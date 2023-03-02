d4c6aa62073f00def37f97bf50d142fa1df2c616
VladKarandashov <vlad.karandashov.tech@gmail.com>
2023-03-02 14:43:02 +0300

    поправил скрипт

--- start-app.sh
+++ start-app.sh
@@ -9,12 +9,16 @@ mvn clean install
 
 echo "Убиваем все приложения"
 tmux kill-server
+sleep 1
+tmux ls
+sleep 2
 
 echo "Запускаем приложение"
 # Запускаем новую сессию tmux в фоновом режиме
 tmux new-session -d -s my-session 'mvn spring-boot:run'
 # Отображаем список сессий tmux
 tmux list-sessions
+tmux ls
 
 echo "Рестартуем nginx"
 sudo systemctl restart nginx
