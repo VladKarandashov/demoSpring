4f83a130addbac6855b91d8902f8293daa44fc80
VladKarandashov <vlad.karandashov.tech@gmail.com>
2023-10-03 21:08:05 +0300

    доп апи

--- src/main/java/ru/abradox/demospring/controller/ApiController.java
+++ src/main/java/ru/abradox/demospring/controller/ApiController.java
@@ -1,13 +1,20 @@
 package ru.abradox.demospring.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.abradox.demospring.controller.rest.SearchRestController;
 
 @Controller
+@RequiredArgsConstructor
 public class ApiController {
 
+    private final SearchRestController searchRestController;
+
     @GetMapping("/api")
-    public String getApiHTML() {
+    public String getApiHTML(Model model) {
+        model.addAttribute("items", searchRestController.getItems(null, null, null));
         return "api";
     }
 }
