d69ee4987dbf6162789c636e217a53c0c4821190
VladKarandashov <vlad.karandashov.tech@gmail.com>
2023-10-03 17:12:33 +0300

    доработка

--- src/main/java/ru/abradox/demospring/controller/RssUtils.java
+++ src/main/java/ru/abradox/demospring/controller/RssUtils.java
@@ -5,13 +5,17 @@ import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 import ru.abradox.demospring.model.entity.NewsItem;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @UtilityClass
 public class RssUtils {
 
@@ -22,8 +26,15 @@ public class RssUtils {
             URL url = new URL(rssLink);
             SyndFeedInput input = new SyndFeedInput();
             SyndFeed feed = input.build(new XmlReader(url));
-
-            for (SyndEntry entry : feed.getEntries()) {
+            CompletableFuture<List<SyndEntry>> future = CompletableFuture.supplyAsync(feed::getEntries);
+            List<SyndEntry> entries;
+            try {
+                entries = future.get(2, TimeUnit.SECONDS);
+            } catch (RuntimeException e) {
+                log.error("Слишком долго получаю данные от "+rssLink);
+                return Collections.emptyList();
+            }
+            for (SyndEntry entry : entries) {
                 var newsItem = NewsItem.builder()
                         .title(entry.getTitle())
                         .description(entry.getDescription().getValue())
@@ -32,13 +43,14 @@ public class RssUtils {
                         .pubDate(entry.getPublishedDate().toString())
                         .build();
                 newsList.add(newsItem);
+                if (newsList.size() >= 5) break;
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Ошибка при парсинге rss по ссылке "+rssLink);
         }
 
         Collections.shuffle(newsList);
 
-        return newsList.stream().limit(5).toList();
+        return newsList;
     }
 }
